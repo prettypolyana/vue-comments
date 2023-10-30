0c7f218b5e694f0ce1cb236cbd0e05395fc64ce2
Polina Kulik <pollyandrrrr@yandex.ru>
2023-10-31 01:48:02 +0300

    comments with a little css

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,94 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="all">
+        <div class="items">
+            <input v-bind:value="title" class="decorate" type="text" placeholder="имя" @input="title = $event.target.value">
+            <input v-bind:value="subtitle" class="decorate" type="text" placeholder="комментарий" @input="subtitle = $event.target.value">
+            <button  @click="createPost" class="btn">добавить</button>
+        </div>
+        <div class="posts" v-for="post in posts">
+            <div class="post"> Имя: {{post.title}} </div>
+            <div class="post"> Комментарий: {{post.subtitle}} </div>
+        </div>
+    </div>
 </template>
 
+<script>
+
+    export default {
+        data() {
+            return {
+                title: '',
+                subtitle: '',
+                posts: [],
+                full: false,
+            }
+        }, 
+        methods: {
+            createPost() {
+                if (this.title.trim() !== '' && this.subtitle.trim() !== '') {
+                    const newPost = {
+                    id: Date.now(),
+                    title: this.title,
+                    subtitle: this.subtitle,
+                    }
+                    this.posts.push(newPost)
+                    this.title = ''
+                    this.subtitle = ''
+                } else {
+                    if (this.title.trim() === '') {
+                        this.title = 'can not be empty'
+                    } 
+                    
+                    if (this.subtitle.trim() === '')
+                        this.subtitle = 'can not be empty'
+                }
+            },
+        }
+    }
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    .all {
+        display: flex;
+        flex-direction: column;
+        gap: 50px;
+    }
 
-nav {
-  padding: 30px;
-}
+    .decorate {
+        height: 40px;
+        border: 2px solid rgb(116, 116, 209);
+    }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    .items {
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+    }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    .btn {
+        width: 100px;
+        align-self: flex-end;
+        height: 30px;
+        background-color: aliceblue;
+        border: 2px solid rgb(116, 116, 209);
+
+    }
+
+    .posts {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        gap: 10px;
+        width: 100%;
+        height: 90px;
+        padding: 10px;
+        border: 2px solid rgb(26, 209, 81);
+
+    }
+
+    .post {
+        border: 1px solid rgb(116, 116, 209);
+        height: 40px;
+    }
 </style>
